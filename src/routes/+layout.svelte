d67963b89ea713d7c2ff3f1cdf936dcc63e4f8b8
Kevin Prom <promkev@gmail.com>
2022-12-29 00:13:56 -0500

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-modern.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+<AppBar>
+	<svelte:fragment slot="lead">
+		Project23
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<button class="btn btn-filled-secondary btn-base">Log in</button>
+		<button class="btn btn-filled-primary btn-base">Sign up</button>
+	</svelte:fragment>
+</AppBar>
+<slot />

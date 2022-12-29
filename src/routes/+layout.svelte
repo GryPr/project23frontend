00365c902688c1c076ef33d42861ca904fc0226e
Kevin Prom <promkev@gmail.com>
2022-12-29 16:15:33 -0500

    Add keycloak

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,28 @@
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
 	import { AppBar } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
+	import { KC_CONFIG, KC_INIT_OPTIONS } from '$lib/auth/config.svelte';
+	import Keycloak from 'keycloak-js';
+
+	function initKeycloak() {
+		let keycloak = new Keycloak(KC_CONFIG)
+		keycloak
+			.init(KC_INIT_OPTIONS)
+			.then(function (authenticated) {
+				alert(authenticated ? 'authenticated' : 'not authenticated');
+			})
+			.catch(function () {});
+	}
+
+	onMount(() => {
+		initKeycloak();
+	});
+
 </script>
+
 <AppBar>
-	<svelte:fragment slot="lead">
-		Project23
-	</svelte:fragment>
+	<svelte:fragment slot="lead">ProjectTitle</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<button class="btn btn-filled-secondary btn-base">Log in</button>
 		<button class="btn btn-filled-primary btn-base">Sign up</button>
